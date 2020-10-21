834940602c016eed711a13de36822b56e09e4476
EitharAlotoom95 <eitharalatoom@gmail.com>
2020-10-21 14:11:01 +0300

    yarn build

---
+++ build/precache-manifest.f412d05c79eaa352e8178b2531ba4b0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba029fc1c94acadede0c",
+    "url": "/static/css/main.ff480302.chunk.css"
+  },
+  {
+    "revision": "ba029fc1c94acadede0c",
+    "url": "/static/js/main.23beab9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a04a84121156c01b6dcf",
+    "url": "/static/js/2.1f62ec98.chunk.js"
+  },
+  {
+    "revision": "d144c3fb4916db0cb5c884ac9413ed80",
+    "url": "/index.html"
+  }
+];
